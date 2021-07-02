ed7ff09c1716f17902737fa4987ac2e2a6424774
senne <senne.rosaer@student.uantwerpen.be>
2021-07-02 15:58:16 +0200

    added videos

---
+++ video_dir/beacon-6-all-types/pull_script.sh
@@ -0,0 +1,5 @@
+echo "machine api.wandb.ai" >> /root/.netrc
+echo "  login user" >> /root/.netrc
+echo "  password a220697dbcaf0bd9df2137876a9b3190835413ad" >> /root/.netrc
+
+wandb pull --entity senne-rosaer --project office-simulator 26bqz2da	
